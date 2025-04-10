b2c5eb25da965f99e89c693755f48a5487e4cb61
Rue Ire <reerine20@msn.com>
2025-04-10 15:28:57 +0300

    update: main.jsx

--- scr/frontend/sound_generator/src/main.jsx
+++ scr/frontend/sound_generator/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
+import './Generator.css'
+import SoundGenerator from './SoundGenerator'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SoundGenerator />
   </StrictMode>,
 )
